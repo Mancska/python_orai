ccb246c07f4994d1ca29ce27b4c96cc4cbcc9c0e
Mancska <95695531+Mancska@users.noreply.github.com>
2023-01-31 11:08:55 +0100

    pontszámitás metodus el készitése

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 #megoldas
-jatekos_laposzeg=0
-gep_laposzeg=0
-def eredmeny():
+def pontszamitas(lapok):
+    ertek= 0
+    for i in range(len(lapok)):
+        ertek= +lapok[i]
+    return ertek
+
+
+def eredmeny(jatekos_laposzeg,gep_laposzeg):
+    gepertek=pontszamitas(gep_laposzeg)
+    jatekosertek=pontszamitas(jatekos_laposzeg)
     if jatekos_laposzeg>21:
         print("vesztet")
     if gep_laposzeg>21:
