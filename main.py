5aa812c467ff686c732bdc130c216b433d46fb91
Mancska <franyokrisz@gmail.com>
2023-02-02 15:14:20 +0100

    ellenözések

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-#megoldas
-def pontszamitas(lapok):
-    ertek= 0
+# megoldas
+def pontokosszege2(lapok: [int]):
+    pontok: int = 0
     for i in range(len(lapok)):
-        ertek= +lapok[i]
-    return ertek
+        pontok += lapok[i]
+
+    return pontok
 
 
-def eredmeny(jatekos_laposzeg,gep_laposzeg):
-    gepertek=pontszamitas(gep_laposzeg)
-    jatekosertek=pontszamitas(jatekos_laposzeg)
-    if jatekos_laposzeg>21:
-        print("vesztet")
-    if gep_laposzeg>21:
-        print("vesztet")
-#tesztesetek
+def eredmeny(gLapok: [int], jLapok: [int]):
+    jPontok: int = pontokosszege2(jLapok)
+    gPontok: int = pontokosszege2(gLapok)
+    vegeredmeny = ""
+    if jPontok and gPontok > 21:
+        vegeredmeny = "mindenki vesztett"
+    elif gPontok > 21:
+        vegeredmeny = "gép vesztett"
+    elif jPontok > 21:
+        vegeredmeny = "játékos vesztett"
+
+    return vegeredmeny
+
+# teszt esetek
+def tesztesetek():
+    jatekos_vesztett_teszt()
+
+def jatekos_vesztett_teszt():
+    jatekos = [10, 9, 3]
+    gep = [10, 6]
+    vart_eredmeny = "játékos vesztett"
+    kapott_eredmeny = eredmeny(gep,jatekos)
+
+    if kapott_eredmeny == vart_eredmeny:
+        print("a teszt sikeres")
+    else:
+        print("a teszt megbukott")
+
+tesztesetek()
